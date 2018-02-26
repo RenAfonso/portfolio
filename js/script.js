5a2b5150e5452277ad9ba7f507a268065a060e98
Renato Afonso <renato.roberto@gmail.com>
2018-02-26 00:12:10 +0000

    desktop

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var menu = document.querySelector('.burguer');
+var main = document.querySelector('main');
+var drawer = document.querySelector('.nav');
+
+menu.addEventListener('click', function(e) {
+	drawer.classList.toggle('open');
+	e.stopPropagation();
+});
+
+main.addEventListener('click', function() {
+	drawer.classList.remove('open');
+});
